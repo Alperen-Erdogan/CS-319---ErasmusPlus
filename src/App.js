1f28276ee1c04e0eba7772fed2b4e6881051235b
AlperenBilkent <alperen.erdogan@ug.bilkent.edu.tr>
2022-12-16 18:36:48 +0300

    Update App.js
    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
-import Content from './Content';
 import Sidebar from './Sidebar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
