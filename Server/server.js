e0a635c83e45cea5b46cc7d14caa4ec4306c226f
Efe Can Tepe <can.tepe@ug.bilkent.edu.tr>
2022-12-18 00:50:36 +0300

    Cors Added

--- Server/server.js
+++ Server/server.js
@@ -7,8 +7,17 @@ const DocumentsStudent = require("./Routes/StudentRoutes/DocumentsStudent")
 const SchoolCourses = require("./Routes/StudentRoutes/SchoolsCourses")
 const Coordinators = require("./Routes/StudentRoutes/Coordinators")
 const ArchiveStudent = require("./Routes/StudentRoutes/ArchiveStudent")
+const cors = require("cors")
 
 const app = express()
+
+app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use(express.json());
 app.use("/MyProfileStudent",MyProfileStudent);
@@ -19,7 +28,22 @@ app.use("/StudentCoordinator", Coordinators)
 app.use("/ArchiveStudent", ArchiveStudent)
 
 app.get("/", (req,res) => {
-    console.log("Entered")
+    res.json({name : "Ahmet"})
+    res.status(200)
+})
+
+app.post("/", (req,res) => {
+
+    console.log(req.body.name)
+    console.log(req.body.surname)
+    console.log(req.body.email)
+    let name = req.body.name
+    let surname = req.body.surname
+    let email = req.body.email
+
+    let userInfo = {"name": name, "surname": surname, "email": email}
+
+    //saveToDabase()
 })
 
 
