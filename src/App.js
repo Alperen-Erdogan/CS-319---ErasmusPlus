1ae10037f3d33d3b4669c303434d8d2290e81fe8
AlperenBilkent <alperen.erdogan@ug.bilkent.edu.tr>
2022-12-17 23:49:22 +0300

    nav setting

--- src/App.js
+++ src/App.js
@@ -12,62 +12,27 @@ import Documents from './Documents'
 import SchoolsAndCourses from './SchoolsAndCourses';
 import CoordinatorList from './CoordinatorList';
 import  Archive from './Archive';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import MyProfilePage from './pages/MyProfilePage';
+import ArchivePage from './pages/ArchivePage';
 
 function App() {
   const pageTitle = "My Profile";
   return (
-    <div className='clear'>
- 
-      <header className="bilkent-header"> {/*Main Header*/}
-        <Row className='row-container-box'>
-          <Col className='row-container-box'>
-            <Row className='row-container-box'>
-              <Container className='container-box'><img src={logo2} className="Bilkent-Logo" alt='bilkent logo'/></Container>
-              <Container className='container-box'><h1 id='bilkent-header-content'>Bilkent University | Erasmus Plus</h1></Container>
-            </Row>
-          </Col>
-          <Col className='header-col'>
-            <Container className='container-box2'><p id='type-id'>User Type</p></Container>
-          </Col>
-        </Row>
-      
-      
-      </header>
-
-      <Container fluid disableGutters={true}>{/*Main Container*/}
-        <Row> {/*Main Row*/}
-
-          <Col className='coltest1'> {/*Navbar column*/}
-          <Sidebar />
-          </Col>
-
-          <Col className='coltest2'> {/*Main Contents Column*/}
-            <Container fluid className='main-content-container'>{/*=================Contents Container================*/}
-
-              <header className='page-title'> {/*Page title*/}
-                <h1 className='page-title-text'>{pageTitle}</h1>
-              </header>
-
-              <Container fluid className='content-container'>
-                <Container fluid className='inner-content-container'>{/*Put contents in there*/ }
-                {/*<p className='App-header'>
-                  <img src={logo} className="App-logo" alt="logo" />
-                  <img src={logo} className="App-logo" alt="logo" />
-                  <img src={logo} className="App-logo" alt="logo" />
-
-                  
-                    Test Bilkentdasdas
-                  </p>*/}
-                  <Archive/>
-
-                </Container>
-              </Container>
-            </Container>{/*=================end of contents========================*/}
-          </Col>
-
-        </Row>
-      </Container>{/*end of main container*/}
-    </div>
+    <>
+    <Router>
+      <Switch>
+        <Route exact path ="/" component = {MyProfilePage} />
+        <Route path="/ArchivePage" component={ArchivePage} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+    </>
   );
 }
 
