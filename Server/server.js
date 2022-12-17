66a82f6f5ec5c8ef0d3850592fabc05d94ee6a90
Efe Can Tepe <can.tepe@ug.bilkent.edu.tr>
2022-12-18 02:00:35 +0300

    Login Page can send data now. Also Email checker is added for valid email in the login

--- Server/server.js
+++ Server/server.js
@@ -10,8 +10,13 @@ const ArchiveStudent = require("./Routes/StudentRoutes/ArchiveStudent")
 const cors = require("cors")
 
 const app = express()
+const bcrypt = require("bcrypt")
+
+const users = [] // Just for test
 
 app.use(cors())
+
+app.use(express.urlencoded({ extended: false}))
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
@@ -26,6 +31,37 @@ app.use("/DocumentStudent", DocumentsStudent)
 app.use("/SchoolCourses", SchoolCourses)
 app.use("/StudentCoordinator", Coordinators)
 app.use("/ArchiveStudent", ArchiveStudent)
+
+// For Logining this is the first thing user will be see
+app.get("/login", (req, res) => {
+    // It might not be usefull at all
+})
+
+app.post("/login", (req, res) => {
+    console.log(req.body.email)
+    console.log(req.body.password)
+})
+
+app.get("/register", (req, res) => {
+
+})
+
+app.post('/register', async (req, res) => {
+    
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now.toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect("/login") // It migth be wrong in the react
+    }catch{
+        res.redirect("/register") // It might be wrong in the react
+    }
+    console.log(users)
+})
 
 app.get("/", (req,res) => {
     res.json({name : "Ahmet"})
