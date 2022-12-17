02d0fe7de33ea2bf3b6a5975c4d0084abcc95733
AlperenBilkent <alperen.erdogan@ug.bilkent.edu.tr>
2022-12-18 02:59:47 +0300

    newapplication page

---
+++ src/pages/NewApplicationPage.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import "../App.css";
+import logo2 from '../items/bilkent-logo.png'
+import Sidebar from '../Sidebar';
+
+const NewApplicationPage = () => {
+  const pageTitle = "New Application"
+  return(
+    <div className='clear'>
+      <header className="bilkent-header"> {/*Main Header*/}
+<Row className='row-container-box'>
+  <Col className='row-container-box'>
+    <Row className='row-container-box'>
+      <Container className='container-box'><img src={logo2} className="Bilkent-Logo" alt='bilkent logo'/></Container>
+      <Container className='container-box'><h1 id='bilkent-header-content'>Bilkent University | Erasmus Plus</h1></Container>
+    </Row>
+  </Col>
+  <Col className='header-col'>
+    <Container className='container-box2'><p id='type-id'>User Type</p></Container>
+  </Col>
+</Row>
+
+
+</header>
+
+<Container fluid>{/*Main Container*/}
+<Row> {/*Main Row*/}
+
+  <Col className='coltest1'> {/*Navbar column*/}
+  <Sidebar />
+  </Col>
+
+  <Col className='coltest2'> {/*Main Contents Column*/}
+    <Container fluid className='main-content-container'>{/*=================Contents Container================*/}
+
+      <header className='page-title'> {/*Page title*/}
+        <h1 className='page-title-text'>{pageTitle}</h1>
+      </header>
+
+      <Container fluid className='content-container'>
+        <Container fluid className='inner-content-container'>{/*Put contents in there*/ }
+
+          
+
+          
+
+        </Container>
+      </Container>
+    </Container>{/*=================end of contents========================*/}
+  </Col>
+
+</Row>
+</Container>{/*end of main container*/}
+    </div>
+  );
+};
+
+export default NewApplicationPage;
