d442390ba63ed57d29b5ae2b2f23c038a3a939ab
denizcbilkent <denizc@ug.bilkent.edu.tr>
2022-12-18 15:39:18 +0300

    Login UI update

--- src/Login.js
+++ src/Login.js
@@ -89,12 +89,12 @@ function Login() {
             <Form>
                 <Form.Group as = {Row} controlId="formBasicEmail">
                     <Col className = "loginCol"><Form.Label style={{marginTop: "5vh"}}>Email</Form.Label></Col>
-                    <Col className = "loginCol"><Form.Control className = "standardTxtInput" type="email" placeholder="Enter email" onChange={e => setEmail(e.target.value)}/></Col>
+                    <Col className = "loginCol"><Form.Control style={{maxWidth:"10vw", minWidth:"11vw"}} className = "standardTxtInput" type="email" placeholder="Enter email" onChange={e => setEmail(e.target.value)}/></Col>
                 </Form.Group>
 
                 <Form.Group as = {Row} controlId="formBasicPassword">
                     <Col className = "loginCol"><Form.Label style={{marginTop: "5vh"}}>Password</Form.Label></Col>
-                    <Col className = "loginCol"><Form.Control className = "standardTxtInput" type="password" placeholder="Password" onChange={event => setPassword(event.target.value)} /></Col>
+                    <Col className = "loginCol"><Form.Control style={{maxWidth:"10vw", minWidth:"11vw"}} className = "standardTxtInput" type="password" placeholder="Password" onChange={event => setPassword(event.target.value)} /></Col>
                 </Form.Group>
                 <Button variant="primary" className = "customBtn" onClick={() => {submit()}} href={href} >
                     Login
