5d3b19f130873393cdfadd6431e064dfa221f9ee
AlperenBilkent <alperen.erdogan@ug.bilkent.edu.tr>
2022-12-19 07:00:52 +0300

    ui update

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { XCircle } from 'react-bootstrap-icons';
 import './Popup.css'
 
 function Popup(props) {
   return (props.trigger) ? (
     <div className='popup'>
-        <div className="popup-inner">
-            <button className="close-btn" onClick={() => props.setTrigger(false)}>close</button>
+        <div className="popup-inner" >
+          <XCircle size={32} className="close-btn x-btn" onClick={() => props.setTrigger(false)}/>
             { props.children }
         </div>
     </div>
