3027c235fab414ffdaff361bad57af1b6e45ce78
denizcbilkent <denizc@ug.bilkent.edu.tr>
2022-12-19 07:14:38 +0300

    Update NewApplication.js

--- src/NewApplication.js
+++ src/NewApplication.js
@@ -60,39 +60,38 @@ function NewApplication() {
             <Row style = {{marginLeft: "1.5vw", fontSize: "18px"}}>
                 <Form>
                     <Form.Group as = {Row} className = "standardTxtInput">
-                        <Col style={{paddingTop: "0.5vh"}}><Form.Label>Application:</Form.Label></Col>
-                        <Col><Form.Control type = "email" placeholder = "Current Semester Date" style = {{maxWidth: "58.5vw", minWidth: "58.5vw"}}/></Col>
+                        <Col style={{paddingTop: "0.5vh",maxWidth: "15%", minWidth: "15%"}}><Form.Label>Application:</Form.Label></Col>
+                        <Col style = {{maxWidth: "85%", minWidth: "85%"}}><Form.Control type = "email" placeholder = "Current Semester Date"/></Col>
                     </Form.Group>
                     <Form.Group as = {Row} className = "standardTxtInput">
-                        <Col style={{paddingTop: "0.5vh"}}><Form.Label>First Name:</Form.Label></Col>
-                        <Col><Form.Control onChange={e => setFirstName(e.target.value)} type = "email" placeholder = "First Name" style = {{maxWidth: "25vw", minWidth: "25vw"}}/></Col>
-                        <Col style={{paddingTop: "0.5vh"}}><Form.Label>Department:</Form.Label></Col>
-                        <Col><Form.Control onChange={e => setDepartment(e.target.value)} type = "email" placeholder = "Department" style = {{maxWidth: "25vw", minWidth: "25vw"}}/></Col>
+                        <Col style={{paddingTop: "0.5vh",maxWidth: "15%", minWidth: "15%"}}><Form.Label>First Name:</Form.Label></Col>
+                        <Col style = {{maxWidth: "35%", minWidth: "35%"}}><Form.Control onChange={e => setFirstName(e.target.value)} type = "email" placeholder = "First Name"/></Col>
+                        <Col style={{paddingTop: "0.5vh",maxWidth: "15%", minWidth: "15%"}}><Form.Label>Department:</Form.Label></Col>
+                        <Col style = {{maxWidth: "35%", minWidth: "35%"}}><Form.Control onChange={e => setDepartment(e.target.value)} type = "email" placeholder = "Department"/></Col>
                     </Form.Group>
                     <Form.Group as = {Row} className = "standardTxtInput">
-                        <Col style={{paddingTop: "0.5vh"}}><Form.Label>Last Name:</Form.Label></Col>
-                        <Col><Form.Control onChange={e => setLastName(e.target.value)} type = "email" placeholder = "Last Name" style = {{maxWidth: "25vw", minWidth: "25vw"}}/></Col>
-                        <Col style={{paddingTop: "0.5vh"}}><Form.Label>CGPA:</Form.Label></Col>
-                        <Col><Form.Control onChange={e => setCgpa(e.target.value)}type = "email" placeholder = "CGPA" style = {{maxWidth: "25vw", minWidth: "25vw"}}/></Col>
+                        <Col style={{paddingTop: "0.5vh",maxWidth: "15%", minWidth: "15%"}}><Form.Label>Last Name:</Form.Label></Col>
+                        <Col style = {{maxWidth: "35%", minWidth: "35%"}}><Form.Control onChange={e => setLastName(e.target.value)} type = "email" placeholder = "Last Name"/></Col>
+                        <Col style={{paddingTop: "0.5vh",maxWidth: "15%", minWidth: "15%"}}><Form.Label>CGPA:</Form.Label></Col>
+                        <Col style = {{maxWidth: "35%", minWidth: "35%"}}><Form.Control onChange={e => setCgpa(e.target.value)}type = "email" placeholder = "CGPA"/></Col>
                     </Form.Group>
                     <Form.Group as = {Row} className = "standardTxtInput">
-                        <Col style={{paddingTop: "0.5vh"}}><Form.Label>Host University:</Form.Label></Col>
-                        <Col><Form.Control onChange={e => setHostUniversity(e.target.value)} type = "email" placeholder = "Host University" style = {{maxWidth: "25vw", minWidth: "25vw"}}/></Col>
-                        <Col style={{paddingTop: "0.5vh"}}><Form.Label>Host Department:</Form.Label></Col>
-                        <Col><Form.Control onChange={e => setHostDepartment(e.target.value)}type = "email" placeholder = "Host Department" style = {{maxWidth: "25'vw", minWidth: "25vw"}}/></Col>
+                        <Col style={{paddingTop: "0.5vh",maxWidth: "15%", minWidth: "15%"}}><Form.Label>Host University:</Form.Label></Col>
+                        <Col style = {{maxWidth: "35%", minWidth: "35%"}}><Form.Control onChange={e => setHostUniversity(e.target.value)} type = "email" placeholder = "Host University"/></Col>
+                        <Col style={{paddingTop: "0.5vh",maxWidth: "15%", minWidth: "15%"}}><Form.Label>Host Department:</Form.Label></Col>
+                        <Col style = {{maxWidth: "35%", minWidth: "35%"}}><Form.Control onChange={e => setHostDepartment(e.target.value)}type = "email" placeholder = "Host Department"/></Col>
                     </Form.Group>
                     <Form.Group as = {Row} className = "standardTxtInput">
                         <Form.Text style = {{paddingLeft: "20vw", paddingRight: "20vw", color: "black"}}>Please upload the documents you downloaded and filled from the Documents section. </Form.Text>
                     </Form.Group>
                     <Form.Group as = {Row} className = "standardTxtInput">
-                        <Col><Form.Label>Documents:</Form.Label></Col>
-                        <Col><Form.Control type="file" multiple style = {{minWidth: "58.5vw", maxWidth: "58.5vw"}}/></Col>
+                        <Col style={{paddingTop: "0.5vh",maxWidth: "15%", minWidth: "15%"}}><Form.Label>Documents:</Form.Label></Col>
+                        <Col style = {{maxWidth: "85%", minWidth: "85%"}}><Form.Control type="file" multiple/></Col>
                     </Form.Group>
                 </Form>
             </Row>
-            <Row>
-                <Col style={{paddingLeft: "3.4vw"}}><Button style={{paddingLeft: "1vw", paddingRight: "1vw", paddingBottom: "0.8vh"}}><ArrowLeft/></Button></Col>
-                <Col style={{paddingLeft: "52.5vw"}}><Button onClick={() => create()} style={{paddingLeft: "1vw", paddingRight: "1vw", paddingBottom: "0.8vh", backgroundColor: "green"}}>Create</Button></Col>
+            <Row style={{marginTop: "1%"}}>
+                <Col style={{paddingLeft: "86%"}}><Button onClick={() => create()} style={{paddingLeft: "1vw", paddingRight: "1vw", paddingBottom: "0.8vh", backgroundColor: "green"}}>Create</Button></Col>
             </Row>               
         </Container>
     </div> 
